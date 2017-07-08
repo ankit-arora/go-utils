83aa3d429dcb607972ed86baaf483a2af7794295
ankit <ankit.j.arora@gmail.com>
2017-07-08 23:37:01 +0530

    better example

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 	//only pool example ends
 
 	//only pipeline example starts
-	pipeline, err := go_pipeline.NewPipeline(10, 1*time.Second, func(data []interface{}) {
+	pipeline, err := go_pipeline.NewPipeline(10, 10*time.Second, func(data []interface{}) {
 		fmt.Println(data)
 	})
 
@@ -42,18 +42,20 @@ func main() {
 	pipeline.Shutdown()
 	//only pipeline example ends
 
-	cp, err := go_concurrent_pipeline.NewConcurrentPipeline(3, 20, 10*time.Second, func(i []interface{}) {
-		fmt.Println(i)
-	})
+	//concurrent pipeline example starts
+	cp, err := go_concurrent_pipeline.NewConcurrentPipeline(10, 21, 10*time.Second,
+		func(i []interface{}) {
+			fmt.Println(i)
+		})
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	for i := 0; i < 1004; i++ {
+	for i := 0; i < 510; i++ {
 		cp.Add(2)
 	}
 	cp.Shutdown()
-
+	//concurrent pipeline example ends
 }
