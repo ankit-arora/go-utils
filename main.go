55d62b430df2b008eb1f7ba651135785152f186e
ankit <ankit.j.arora@gmail.com>
2017-07-08 18:14:07 +0530

    better example

--- main.go
+++ main.go
@@ -3,23 +3,23 @@ package main
 import (
 	"fmt"
 	"goroutines-pool/goroutines-pool"
+	"strconv"
 )
 
 func main() {
-	p, err := goroutines_pool.NewPool(3)
+	p, err := goroutines_pool.NewPool(5)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	p.Start()
-	p.Submit(func() {
-		fmt.Println("task 1")
-	})
-	p.Submit(func() {
-		fmt.Println("task 2")
-	})
-	p.Submit(func() {
-		fmt.Println("task 3")
-	})
+
+	for i := 0; i <= 10; i++ {
+		j := i
+		p.Submit(func() {
+			fmt.Println("task " + strconv.Itoa(j))
+		})
+	}
+
 	p.Stop()
 }
