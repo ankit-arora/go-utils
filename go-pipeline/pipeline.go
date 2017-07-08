9e9700f513df9c196d3b78690c3be2de6326d5b3
ankit <ankit.j.arora@gmail.com>
2017-07-08 20:41:21 +0530

    pipeline added

---
+++ go-pipeline/pipeline.go
@@ -0,0 +1,68 @@
+package go_pipeline
+
+import "time"
+
+type Pipeline struct {
+	pipe         chan interface{}
+	pipeSize     int
+	timeout      time.Duration
+	pipeLineTask func([]interface{})
+	shutdown     chan struct{}
+	wait         chan struct{}
+}
+
+func (p *Pipeline) Add(d interface{}) {
+	p.pipe <- d
+}
+
+func (p *Pipeline) start() {
+	go func() {
+		var t *time.Timer
+		defer t.Stop()
+		var state []interface{}
+		forBreak := false
+		for !forBreak {
+			t = time.NewTimer(p.timeout)
+			select {
+			case d := <-p.pipe:
+				state = append(state, d)
+				if len(state) == p.pipeSize {
+					//fmt.Println("from data")
+					p.pipeLineTask(state)
+					state = nil
+				}
+			case <-t.C:
+				if len(state) > 0 {
+					//fmt.Println("from timeout")
+					p.pipeLineTask(state)
+					state = nil
+				}
+			case <-p.shutdown:
+				if len(state) > 0 {
+					//fmt.Println("from shutdown")
+					p.pipeLineTask(state)
+					state = nil
+				}
+				forBreak = true
+			}
+		}
+		p.wait <- struct{}{}
+	}()
+}
+
+func (p *Pipeline) Shutdown() {
+	close(p.shutdown)
+	<-p.wait
+}
+
+func NewPipeline(pipeSize int, timeout time.Duration, f func([]interface{})) (*Pipeline, error) {
+	p := &Pipeline{}
+	p.pipeSize = pipeSize
+	p.timeout = timeout
+	p.pipeLineTask = f
+	p.pipe = make(chan interface{})
+	p.shutdown = make(chan struct{})
+	p.wait = make(chan struct{})
+	p.start()
+	return p, nil
+}
--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"goroutines-pool/go-pipeline"
 	"goroutines-pool/goroutines-pool"
 	"strconv"
+	"time"
 )
 
 func main() {
+	//only pool example starts
 	p, err := goroutines_pool.NewPool(5)
 	if err != nil {
 		fmt.Println(err)
@@ -22,4 +25,20 @@ func main() {
 	}
 
 	p.Stop()
+	//only pool example ends
+
+	//only pipeline example starts
+	pipeline, err := go_pipeline.NewPipeline(10, 1*time.Second, func(data []interface{}) {
+		fmt.Println(data)
+	})
+
+	for i := 0; i < 52; i++ {
+		pipeline.Add(1)
+	}
+
+	//time.Sleep(1 * time.Minute)
+
+	pipeline.Shutdown()
+	//only pipeline example ends
+
 }
